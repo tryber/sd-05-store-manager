0a0b4b5bae1d257112fef40b0e13a3bb4425ca61
Mau <mauricio.sdjr@gmail.com>
2020-11-26 16:09:32 -0300

    setup feito, pastas criadas e dependencias instaladas

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
 app.get('/', (request, response) => {
   response.send();
 });
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}`));
---
+++ models/connection.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+
+let connection;
+
+const DB_NAME = 'StoreManager';
+const MONGO_DB_URL = `mongodb://mongodb:27017/${DB_NAME}`;
+
+async function getCollection(collectionName) {
+  connection = connection
+  || (await MongoClient.connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }));
+
+  return connection.db(DB_NAME).collection(collectionName);
+}
+
+module.exports = getCollection;
