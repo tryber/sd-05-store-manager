05dc2f2f0b595a2e25cf4f7ec75f40cbbe84bfa9
Rafael Assad <rafaassadeng@gmail.com>
2020-11-29 12:26:53 -0300

    1 fixed avaliador correto lint

--- src/models/addProduct.js
+++ src/models/addProduct.js
@@ -2,8 +2,7 @@ const getCollection = require('./mongo');
 
 const create = async (name, quantity) => {
   const product = await getCollection('products').then((collection) =>
-    collection.insertOne({ name, quantity }),
-  );
+    collection.insertOne({ name, quantity }));
   return { _id: product.insertedId, name, quantity };
 };
 
