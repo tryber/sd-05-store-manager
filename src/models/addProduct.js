398d3cd390c30e564333009f9a34f164d6e3a465
Rafael Assad <rafaassadeng@gmail.com>
2020-11-29 13:02:02 -0300

    2 lint

--- src/models/addProduct.js
+++ src/models/addProduct.js
@@ -3,8 +3,7 @@ const getCollection = require('./mongo');
 
 const create = async (name, quantity) => {
   const product = await getCollection('products').then((collection) =>
-    collection.insertOne({ name, quantity }),
-  );
+    collection.insertOne({ name, quantity }));
   return { _id: product.insertedId, name, quantity };
 };
 
