eeb8a54cca6cf1c4cf612a00cbc176e65a9135f6
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-12-01 02:15:07 -0300

    Inicia a configuração para rodar o servidor na porta 3000

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`The Door is ${PORT}, who are you?`);
 });
