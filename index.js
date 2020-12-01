2d129ce92014f83a7d05cc4411ec8a1a1a0b6415
binhus <eng.robsonwilli@gmail.com>
2020-12-01 15:55:59 -0300

    atualizando a porta

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const routes = require('./routes/index');
 require('dotenv').config();
 
 const app = express();
-const { PORT } = process.env || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(parser.json());
 // não remova esse endpoint, e para o avaliador funcionar
