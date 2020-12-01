2bf8aa4bff4162e7b9012f037ea47e7da23bd6fe
Giuliano Peixoto <giulianopeixoto@gmail.com>
2020-12-01 17:58:57 -0300

    correção em model

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,17 +1,17 @@
 const storeCollection = require('./connection');
 
 const getAll = async () => {
-  storeCollection('products').then((products) => products.find().toArray());
+  storeCollection('products')
+    .then((products) => products.find().toArray());
 };
 
 const nameProd = async (name) =>
-  storeCollection()
-    .then((db) => db.collection('products'))
+  storeCollection('products')
     .then((products) => products.findOne({ name }));
 
 const create = async (name, quantity) =>
-  storeCollection()
-    .then((db) => db.collection('products').insertOne({ name, quantity }))
+  storeCollection('products')
+    .then((result) => result.insertOne({ name, quantity }))
     .then((prod) => ({ _id: prod.insertedId, name, quantity }));
 
 module.exports = {
