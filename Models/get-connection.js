3ca0ffc20c957dc8faef6cd7fbf9acd0dd1728f9
hleoc <informig@gmail.com>
2020-12-02 16:55:01 -0300

    erro no connection

--- Models/get-connection.js
+++ Models/get-connection.js
@@ -1,7 +1,8 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const DB_NAME = 'StoreManager';
-const MONGO_DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://mongodb:27017/${DB_NAME}`;
 
 let connection;
 
