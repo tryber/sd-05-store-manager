d2b5cba5bb3ee7fd5847ab753a011ad274565046
Virginia Duca <virginiambd.bio@gmail.com>
2020-12-02 13:51:19 -0300

    consertando msg de erro

--- services/catchError.js
+++ services/catchError.js
@@ -1,11 +1,15 @@
 module.exports = (name, quantity) => {
   if (name.length < 5) {
-    throw { code: 'invalid_data', message: '"name" length must be at least 5 characters long' };
+    throw {
+      err: { code: 'invalid_data', message: '"name" length must be at least 5 characters long' },
+    };
   }
   if (quantity <= 0) {
-    throw { code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' };
+    throw {
+      err: { code: 'invalid_data', message: '"quantity" must be larger than or equal to 1' },
+    };
   }
   if (typeof quantity === 'string') {
-    throw { code: 'invalid_data', message: '"quantity" must be a number' };
+    throw { err: { code: 'invalid_data', message: '"quantity" must be a number' } };
   }
 };
