f398d9e9ce7fe4dc21734af1adf2df0796b5e2b9
Preto115 <preto115@gmail.com>
2020-12-01 22:17:25 -0300

    implementando middlewares de validacao e requisito de update

---
+++ middlewares/validateProduct.js
@@ -0,0 +1,53 @@
+const rescue = require('express-rescue');
+const { getByName } = require('../models/productsModels');
+
+module.exports = rescue(async (req, _res, next) => {
+  const { name, quantity } = req.body;
+  console.log(name);
+  if (name.length < 5) {
+    next({
+      err: {
+        code: 'invalid_data',
+        message: '"name" length must be at least 5 characters long',
+      },
+    });
+  }
+
+  if (quantity <= 0) {
+    next({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be larger than or equal to 1',
+      },
+    });
+  }
+
+  if (typeof quantity === 'string') {
+    throw next({
+      err: {
+        code: 'invalid_data',
+        message: '"quantity" must be a number',
+      },
+    });
+  }
+
+  const nameExists = await getByName(name);
+  if (nameExists) {
+    next({
+      err: {
+        code: 'invalid_data',
+        message: 'Product already exists',
+      },
+    });
+  }
+
+  if (!name || !quantity) {
+    next({
+      err: {
+        code: 'invalid_data',
+        message: 'Name and quantity are required',
+      },
+    });
+  }
+  next();
+});
