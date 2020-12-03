5682c0ec34f3a24562c9084f864fd86c4c8cccaa
César Alves <alvescesar@hotmail.com>
2020-12-03 00:08:16 -0300

    Burning down the store...

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-// não remova esse endpoint, e para o avaliador funcionar
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+
+app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
