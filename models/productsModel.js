180f7d5d74e1aefa1580666dc81e26a2a2886865
marinabrcls <barcelos.marina@gmail.com>
2020-12-20 10:31:11 -0300

    várias fita né

--- models/productsModel.js
+++ models/productsModel.js
@@ -1,21 +1,21 @@
-const getConnection = require('./connection');
+const getCollection = require('./connection');
 
 const createProduct = async (name, quantity) => {
-  const db = await getConnection('products');
+  const db = await getCollection('products');
   const newProduct = await db.insertOne({ name, quantity });
   console.log(newProduct);
   return { _id: newProduct.insertedId, name, quantity };
 };
 
-const findByProductName = async (collection, name) => {
-  const db = await getConnection(collection);
+const findByProductName = async (name) => {
+  const db = await getCollection('products');
   const result = await db.findOne({ name });
 
   return result;
 };
 
 const getAll = async () => {
-  const db = await getConnection('products');
+  const db = await getCollection('products');
   const allProducts = await db.find().toArray();
   return allProducts;
 };
