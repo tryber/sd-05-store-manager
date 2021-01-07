735dbcc8cb6efaef13680e3e3a70aa583fd0425b
ederusena <eder.sena@live.com>
2021-01-07 00:40:44 -0300

    Requisito 1-2

--- services/productService.js
+++ services/productService.js
@@ -3,22 +3,26 @@ const productModel = require('../models/productModel');
 // As funções aqui chamam as funções do Model para efetivar as mudanças no BD.
 
 const isValid = async (name, quantity) => {
-  // [Será validado que não é possível criar um produto com o nome menor que 5 caracteres] erro status http 422:
+  // [Será validado que não é possível criar um produto com o nome menor
+  // que 5 caracteres] erro status http 422:
   if (name.length < 5) {
     throw {
       code: 'invalid_data',
       message: '"name" length must be at least 5 characters long',
     };
   }
-  // [Será validado que não é possível criar um produto com quantidade menor que zero] erro status http 422:
-  // [Será validado que não é possível criar um produto com quantidade igual a zero] erro status http 422:
+  // [Será validado que não é possível criar um produto com quantidade menor
+  // que zero] erro status http 422:
+  // [Será validado que não é possível criar um produto com quantidade igual
+  // a zero] erro status http 422:
   if (quantity <= 0) {
     throw {
       code: 'invalid_data',
       message: '"quantity" must be larger than or equal to 1',
     };
   }
-  // [Será validado que não é possível criar um produto com uma string no campo quantidade] erro status http 422:
+  // [Será validado que não é possível criar um produto com uma string no
+  // campo quantidade] erro status http 422:
   if (typeof quantity !== 'number') {
     throw {
       code: 'invalid_data',
@@ -31,7 +35,8 @@ const isValid = async (name, quantity) => {
 const create = async (name, quantity) => {
   const validProduct = await isValid(name, quantity);
   if (!validProduct) return false;
-  // [Será validado que não é possível criar um produto com o mesmo nome de outro já existente] erro status http 422:
+  // [Será validado que não é possível criar um produto com o mesmo nome de
+  // outro já existente] erro status http 422:
   const productExists = await productModel.findProduct(name);
   if (productExists) {
     throw {
@@ -39,7 +44,8 @@ const create = async (name, quantity) => {
       message: 'Product already exists',
     };
   }
-  return await productModel.create(name, quantity);
+  const productCreated = await prodModel.create(name, quantity);
+  return productCreated;
 };
 
 module.exports = { create };
