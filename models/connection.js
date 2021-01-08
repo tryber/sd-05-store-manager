e6a3ca2ddfc28b5e633d8299665b2b5065ed7a0a
ederusena <eder.sena@live.com>
2021-01-08 11:14:36 -0300

    Requisito 3 e 4

--- models/connection.js
+++ models/connection.js
@@ -3,8 +3,8 @@ require('dotenv').config();
 
 const DB_NAME = 'StoreManager';
 
-const MONGO_DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
-// const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://mongodb:27017/${DB_NAME}`;
+// const MONGO_DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || `mongodb://mongodb:27017/${DB_NAME}`;
 
 let connection = null;
 const getConnection = async (collectionName) => {
