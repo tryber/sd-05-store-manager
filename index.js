de3320391b1a7451a18217a3e960a9f128de4e09
carlanakajuni <carlanakajuni@hotmail.com>
2021-01-17 18:26:59 -0300

    Carla: Store Manager

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
   response.send();
 });
+// ------------------------------------------------------
