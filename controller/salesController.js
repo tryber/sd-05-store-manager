abd7029c080566eb2e5384581e1fb1aaa55784db
carlanakajuni <carlanakajuni@hotmail.com>
2021-01-23 19:52:20 -0300

    Requisito 6. Metodo GET para listar os sales

--- controller/salesController.js
+++ controller/salesController.js
@@ -1,10 +1,10 @@
 const { Router } = require('express');
-// const { ObjectId } = require('mongodb');
 const rescue = require('express-rescue');
 const sales = require('../models/sales');
 
 const salesRouter = Router();
 const checkSale = require('../middleware/middleware_sales');
+const { ObjectID } = require('mongodb');
 
 salesRouter.post('/', checkSale, rescue(async (req, res) => {
   const arraySales = req.body;
@@ -13,5 +13,36 @@ salesRouter.post('/', checkSale, rescue(async (req, res) => {
 
   return res.status(200).json(sale);
 }));
+
+salesRouter.get('/:id', async (req, res) => {
+  
+  if (!ObjectID.isValid(req.params.id)) {
+    return res.status(404).json({
+      err: {
+        code: 'not_found',
+        message: 'Sale not found',
+      },
+    });
+  }
+  
+  const saleById = await sales.getSaleById(req.params.id);
+  
+  if (!saleById) {
+    return res.status(404).json({
+      err: {
+        code: 'not_found',
+        message: 'Sale not found',
+      },
+    });
+  }
+
+  return res.status(200).json(saleById);
+})
+
+salesRouter.get('/', async (req, res) => {
+  const listSales = await sales.listSales()
+
+  return res.status(200).json(listSales);
+})
 
 module.exports = salesRouter;
--- models/sales.js
+++ models/sales.js
@@ -4,10 +4,19 @@
 const connectionDB = require('./connection');
 
 const salesCreate = async (itensSold) => connectionDB('sales')
-  .then((db) => db.insertOne({ batatinha: itensSold }))
+  .then((db) => db.insertOne({ itensSold }))
   .then((result) => ({
     _id: result.insertedId,
     itensSold,
   }));
 
-module.exports = { salesCreate };
+const listSales = async () => connectionDB('sales')
+  .then((db) => db.find({}).toArray())
+  .then((result) => ({
+    sales: result,
+  }));
+
+const getSaleById = async (id) => connectionDB('sales')
+  .then((db) => db.find({ _id: id }));
+
+module.exports = { salesCreate, listSales, getSaleById };
