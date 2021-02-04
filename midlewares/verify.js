4fb1d58957a5445c6d74d7b54284d56d9d122f1f
Sidnei Ramos <sidneiramos02@gmail.com>
2021-02-04 08:02:24 -0300

    Requisito 5 concluido.

--- midlewares/verify.js
+++ midlewares/verify.js
@@ -12,13 +12,6 @@ function verifyName(name) {
 
 function verifyQuantity(quantity) {
   const err = { isErr: false };
-  if (quantity <= 0) {
-    err.isErr = true;
-    err.message = '"quantity" must be larger than or equal to 1';
-    err.status = 422;
-    err.code = 'invalid_data';
-    throw err;
-  }
   if (typeof quantity === 'string') {
     err.isErr = true;
     err.message = '"quantity" must be a number';
@@ -26,7 +19,26 @@ function verifyQuantity(quantity) {
     err.code = 'invalid_data';
     throw err;
   }
+  if (quantity <= 0) {
+    err.isErr = true;
+    err.message = '"quantity" must be larger than or equal to 1';
+    err.status = 422;
+    err.code = 'invalid_data';
+    throw err;
+  }
   return err;
 }
 
-module.exports = { verifyName, verifyQuantity };
+function verifyQuantitySales(quantity) {
+  const err = { isErr: false };
+  if (typeof quantity === 'string' || quantity <= 0) {
+    err.isErr = true;
+    err.message = 'Wrong product ID or invalid quantity';
+    err.status = 422;
+    err.code = 'invalid_data';
+    throw err;
+  }
+  return err;
+}
+
+module.exports = { verifyName, verifyQuantity, verifyQuantitySales };
--- services/sales.js
+++ services/sales.js
@@ -1,7 +1,10 @@
 // const { verifyName, verifyQuantity } = require('../midlewares/verify');
+const { forEachChild } = require('typescript');
 const modelSales = require('../models/sales');
+const { verifyQuantitySales } = require('../midlewares/verify');
 
 const insertNewSale = async (itensSold) => {
+  itensSold.forEach((array) => verifyQuantitySales(array.quantity));
   const newSale = await modelSales.insertNewSale(itensSold);
   return newSale;
 };
