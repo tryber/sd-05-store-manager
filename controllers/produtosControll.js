9e478b55617ea7dcbc9822bdae58f666bff14348
Larissa Palombo <larissampalombo@gmail.com>
2021-02-04 17:58:07 -0300

    connection

--- controllers/produtosControll.js
+++ controllers/produtosControll.js
@@ -4,5 +4,18 @@
 const { Router } = require('express');
 const servicos = require('../services/produtosService');
 
-const productsRoute = Router();
-produ
+const products = Router();
+products.post('/', async (req, res) => {
+    try {
+        const { name, quantity } = req.body;
+        const new = await servicos.create(name, quantity);
+        res.status(201).json(new);
+    } catch (err) {
+        if (err.code === 'invalid_data') {
+            return res.status(422).json(err.message);
+        }
+        res.status(500).json({ message: 'Algo deu errado.' });
+    }
+});
+
+module.exports = products;
--- models/connection.js
+++ models/connection.js
@@ -1,14 +1,15 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/StoreManager';
+const MONGO_DB_URL = `mongodb://localhost:27017/${DB_NAME}`;
 const DB_NAME = 'StoreManager';
+
 let connection = null;
 
 module.exports = async (collectionName) => {
-  const OPTIONS = {
+  connection = connection || await MongoClient.connect(MONGO_DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  };
-  connection = connection || (await MongoClient.connect(MONGO_DB_URI, OPTIONS));
+  });
+
   return connection.db(DB_NAME).collection(collectionName);
 };
