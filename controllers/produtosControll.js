9cf853610c58eb4fca411e32dc4a14b616a65858
Larissa Palombo <larissampalombo@gmail.com>
2021-02-05 15:59:20 -0300

    agora ta ouvindo na porta

--- controllers/produtosControll.js
+++ controllers/produtosControll.js
@@ -2,22 +2,24 @@
 // "garçom" do restaurante
 
 const { Router } = require('express');
-const servicos = require('../services/produtosService');
+const services = require('../services/produtosService');
 
 const products = Router();
+
 products.post('/', async (req, res) => {
   try {
     const { name, quantity } = req.body;
-    const newProduct = await servicos.create(name, quantity);
-    res.status(201).json(newProduct);
+
+    const newProdutct = await services.create(name, quantity);
+
+    res.status(201).json(newProdutct);
   } catch (err) {
     if (err.code === 'invalid_data') {
-      return res.status(422).json({ err: { code: err.code, message: err.message } });
+      return res.status(422).json({ err: { code: err.code, message: err.message }});
     }
+
     res.status(500).json({ message: 'Algo deu errado.' });
   }
 });
 
-module.exports = {
-  products,
-};
+module.exports = products;
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // bodyparser é tradução das requisições para algo que consegue ler
 const express = require('express');
 const bodyParser = require('body-parser');
-// const middlewares = require('./middlewares');
 const produtosControll = require('./controllers/produtosControll');
 
 const app = express();
+
 app.use(bodyParser.json());
 
 // não remova esse endpoint, e para o avaliador funcionar
@@ -13,10 +13,13 @@ app.get('/', (request, response) => {
 });
 
 app.use('/products', produtosControll);
+
 app.use((err, _req, res, _next) => {
   console.log(err);
   res.status(500).json({ message: err.message });
 });
 
 const PORT = 3000;
-app.listen(PORT, console.log(`Bora abrir a loja? ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Bora abrir a loja? ${PORT}`);
+});
