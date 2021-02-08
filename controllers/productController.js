499a44fa098f1e817e33e839518e4deaced6dbb5
Larissa Palombo <larissampalombo@gmail.com>
2021-02-08 20:42:51 -0300

    try catch

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,68 +12,88 @@ const productRoute = Router();
 productRoute.post(
   '/',
   rescue(async (req, res) => {
-    const { name, quantity } = req.body;
+    try {
+      const { name, quantity } = req.body;
 
-    const response = await services.create(name, quantity);
+      const response = await services.create(name, quantity);
 
-    if (response.err && response.err.code === 'invalid_data') {
-      return res.status(422).json(response);
+      if (response.err && response.err.code === 'invalid_data') {
+        return res.status(422).json(response);
+      }
+
+      return res.status(201).json(response);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-
-    return res.status(201).json(response);
   }),
 );
 
 productRoute.get(
   '/',
   rescue(async (_req, res) => {
-    const products = await productModel.showAll();
+    try {
+      const products = await productModel.showAll();
 
-    return res.status(200).json({ products });
+      return res.status(200).json({ products });
+    } catch (e) {
+      res.status(500).json(e.message);
+    }
   }),
 );
 
 productRoute.get(
   '/:id',
   rescue(async (req, res) => {
-    const { id } = req.params;
+    try {
+      const { id } = req.params;
 
-    const response = await services.showById(id);
-    if (response.err && response.err.code === 'invalid_data') {
-      return res.status(422).json(response);
+      const response = await services.showById(id);
+      if (response.err && response.err.code === 'invalid_data') {
+        return res.status(422).json(response);
+      }
+      return res.status(200).json(response);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-    return res.status(200).json(response);
   }),
 );
 
 productRoute.put(
   '/:id',
   rescue(async (req, res) => {
-    const { id } = req.params;
-    const { name, quantity } = req.body;
+    try {
+      const { id } = req.params;
+      const { name, quantity } = req.body;
 
-    const response = await services.atualizar(id, name, quantity);
+      const response = await services.atualizar(id, name, quantity);
 
-    if (response.err && response.err.code === 'invalid_data') {
-      return res.status(422).json(response);
+      if (response.err && response.err.code === 'invalid_data') {
+        return res.status(422).json(response);
+      }
+
+      return res.status(200).json(response);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-
-    return res.status(200).json(response);
   }),
 );
 
 productRoute.delete(
   '/:id',
   rescue(async (req, res) => {
-    const { id } = req.params;
+    try {
+      const { id } = req.params;
 
-    const response = await services.excluir(id);
+      const response = await services.excluir(id);
 
-    if (response.err && response.err.code === 'invalid_data') {
-      return res.status(422).json(response);
+      if (response.err && response.err.code === 'invalid_data') {
+        return res.status(422).json(response);
+      }
+
+      return res.status(200).json(response);
+    } catch (e) {
+      res.status(500).json(e.message);
     }
-
-    return res.status(200).json(response);
   }),
 );
 
