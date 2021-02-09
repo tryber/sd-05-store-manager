78b1bc4e0d785bcac17cd7066a6e304c76d60921
saturnian99 <saturnian.dsg@gmail.com>
2021-02-09 12:55:59 -0300

    Created CRUD for products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -4,10 +4,29 @@ const products = Router();
 
 const {
   registerProduct,
-} = require('../services/product.services');
+  listProduct,
+  updateProduct,
+  deleteProduct,
+} = require('../services/product.service');
+
+products.get('/', listProduct, (req, res) => {
+  res.status(200).json(req.data);
+});
+
+products.get('/:id', listProduct, (req, res) => {
+  res.status(200).json(req.data);
+});
 
 products.post('/', registerProduct, (req, res) => {
   res.status(201).json(req.data);
+});
+
+products.put('/:id', updateProduct, (req, res) => {
+  res.status(200).json(req.data);
+});
+
+products.delete('/:id', deleteProduct, (req, res) => {
+  res.status(200).json(req.data);
 });
 
 module.exports = products;
